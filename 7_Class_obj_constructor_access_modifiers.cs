a5263c0a0d32320e8690f73c7ae0a1025d51e3ff
PrachiKarle2323 <prachikarle2323@gmail.com>
2025-12-30 19:46:56 +0530

    Update and rename 7_Class_obj_constructor.cs to 7_Class_obj_constructor_access_modifiers.cs

--- 7_Class_obj_constructor_access_modifiers.cs
+++ 7_Class_obj_constructor_access_modifiers.cs
@@ -1,6 +1,11 @@
 using System;
 
 public class Program{
+    //private
+   int age=23;
+   //protected
+   protected int a=3;
+   //public
    public string name;
    public Program(string n){
        name=n;
@@ -15,5 +20,6 @@ public class Demo{
         Program p=new Program("Prachi");
         p.Method1();
         Console.WriteLine(p.name);
+        // Console.WriteLine(p.age);//err
     }
 }
