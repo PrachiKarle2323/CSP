afed40b2ced916508705d0ea615305f180e669c4
PrachiKarle2323 <prachikarle2323@gmail.com>
2025-12-30 20:45:19 +0530

    Update 12_Interface.cs

--- 12_Interface.cs
+++ 12_Interface.cs
@@ -16,3 +16,30 @@ class Demo{
       v.start();
     }
 }
+
+//Multiple interfaces
+using System;
+
+interface IVehicle{
+    void start();
+}
+interface IVehicle1{
+    void end();
+}
+class Car:IVehicle,IVehicle1{
+    public void start(){
+        Console.WriteLine("Car started!");
+    }
+    public void end(){
+        Console.WriteLine("Car stop!");
+    }
+}
+
+class Demo{
+    static void Main(){
+      IVehicle v=new Car();
+      v.start();
+      IVehicle1 v1=new Car();
+      v1.end();
+    }
+}
